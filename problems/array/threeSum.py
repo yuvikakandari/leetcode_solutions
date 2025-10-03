abdc2926f3ef1f2306e65f888f06e8aa3df9bbba
Yuvika Kandari <yuvika7kandari@gmail.com>
2025-10-03 23:43:13 +0530

    update

--- problems/array/threeSum.py
+++ problems/array/threeSum.py
@@ -43,4 +43,3 @@ if __name__ == "__main__":
     s = Solution()
     print(s.threeSum([-1,0,1,2,-1,-4]))   # -> [[-1,-1,2], [-1,0,1]]
     print(s.threeSum([0,0,0,0]))          # -> [[0,0,0]]
-    print(s.threeSum([]))                 # -> []
